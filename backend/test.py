d438810e718e939defd4ec411a9deee56d582ef1
Ahmed Raza Shaikh <114650047+ahmedraza1234567@users.noreply.github.com>
2025-10-16 02:30:17 +0530

    Remove user registration function and calls

--- backend/test.py
+++ backend/test.py
@@ -1,11 +1,1 @@
-def register(username,email,password):
-    print(username,email, password)
-    print("User registered successfully")
 
-username=input("enter username")
-email=input("enter email")
-password=input("enter password")
-
-
-
-register(username,email, password)
